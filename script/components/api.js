7458ef386bea9e1fc12c2fde0fe1fc15a1175a63
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-09 10:59:15 +0100

    Update index.html
    Update to index.html about api to bio read

--- script/components/api.js
+++ script/components/api.js
@@ -3,3 +3,16 @@
 
 // API to bio read 
 
+async function api(){
+    try{
+        const response = await fetch(urlBio);
+        const result = await response.json();
+
+        console.log(result);
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+api()
