2577c8dc723672c0d6ce465375504b85e0160914
Kent Erik Hole <kent.erik.hole@outlook.com>
2022-12-09 11:30:47 +0100

    Update function.js
    for the heading to type speed

--- script/components/function.js
+++ script/components/function.js
@@ -12,3 +12,17 @@
 
     }
 
+// Text Readting function for add text one letter at the time
+
+    function typeSpeed(value1, value2){
+        // if length is great then length of value1
+        if(t < value1.lenght){
+            // value 2 is the dom to add to 
+            value2.innerHTML += value1.charAt(t);
+            // charAT is build inn function on js
+            t++
+            // this will sett the speed off typing
+            setTimeout(typeSpeed, speedOfType);
+        }
+    }
+
